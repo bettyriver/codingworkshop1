28d142be7858f96062cf89219220afb7e176aa12
Yifan Mai <yifan.mai@sydney.edu.au>
2023-03-20 15:12:12 +1100

    update sky_sim.py

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -12,36 +12,64 @@ from math import cos, pi
 from random import uniform
 
 
-# from wikipedia
-RA = '00:42:44.3'
-DEC = '41:16:09'
+def get_radec():
+    '''
+    Determine Andromeda location in ra/dec degress
 
-# convert to decimal degrees
+    Returns
+    -------
+    ra : TYPE
+        DESCRIPTION.
+    dec : TYPE
+        DESCRIPTION.
+
+    '''
+    # from wikipedia
+    RA = '00:42:44.3'
+    DEC = '41:16:09'
+    
+    # convert to decimal degrees
+    
+    
+    d, m, s = DEC.split(':')
+    dec = int(d)+int(m)/60+float(s)/3600
+    
+    h, m, s = RA.split(':')
+    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = ra/cos(dec*pi/180)
+    return (ra,dec)
 
 
-d, m, s = DEC.split(':')
-dec = int(d)+int(m)/60+float(s)/3600
+def make_stars(ra,dec,num_stars):
+    
+    '''return num_stars within 1 deg of Andromeda'''
 
-h, m, s = RA.split(':')
-ra = 15*(int(h)+int(m)/60+float(s)/3600)
-ra = ra/cos(dec*pi/180)
+
+
+    
+    
+    # make 1000 stars within 1 degree of Andromeda
+    
+    ras = []
+    decs = []
+    for i in range(num_stars):
+        ras.append(ra + uniform(-1,1))
+        decs.append(dec + uniform(-1,1))
+    return (ras,decs)
 
 nsrc = 1000000
 
-# make 1000 stars within 1 degree of Andromeda
+if __name__=='__main__':
 
-ras = []
-decs = []
-for i in range(nsrc):
-    ras.append(ra + uniform(-1,1))
-    decs.append(dec + uniform(-1,1))
-
-
-# now write these to a csv file for use by my other program
-f = open('catalog.csv','w')
-print("id,ra,dec", file=f)
-for i in range(nsrc):
-    print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
+    ra, dec = get_radec()
+    ras, decs = make_stars(ra, dec, num_stars=nsrc)
+    
+    
+    # now write these to a csv file for use by my other program
+    with open('catalog.csv','w', encoding='utf-8') as f:
+        print("id,ra,dec", file=f)
+        for i in range(nsrc):
+            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
     
     
 # test
